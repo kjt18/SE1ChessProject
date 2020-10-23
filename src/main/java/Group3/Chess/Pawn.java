000ddb9c4b9048fbff88a7305fa9fa1b83f354b5
Aaron Wattenphul <aw160@students.uwf.edu>
2020-10-23 11:25:38 -0500

    modified pawn

--- src/main/java/Group3/Chess/Pawn.java
+++ src/main/java/Group3/Chess/Pawn.java
@@ -11,9 +11,24 @@ public class Pawn extends Piece {
 	public Image getImage() {
 		return new Image(getColor() == "white" ? "pawn_white.png" : "pawn_black.png");
 	}
+	private int moveValue = 2;
+	if (this.getSquare().getY() - 1 > 1){
+		moveValue = 1;
+	}
 	public List<Square> getMoves() {
 		List<Square> squares = new ArrayList<Square>();		
-		//TODO create pawn's move set
+		//finds the possible squares downwards
+		for (int i = getSquare().getY() - 1; i >= 0 && getSquare().getY() + i < moveValue; i--) {		
+			Square square = getBoard().getSquare(getSquare().getX(), i);
+			if (square.getPiece() == null) {
+				squares.add(square);
+			} else if (square.getPiece().getColor().compareTo(getColor()) != 0) {
+				squares.add(square);
+				break;
+			} else {
+				break;
+			}
+		}	
 		return squares;
 	}
 }
