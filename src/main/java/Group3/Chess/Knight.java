a4504de4670da3004be3d70a488db8a33579d63e
Johnny Mitchell <jmitchell3@uwf.edu>
2020-10-30 14:24:39 -0500

    Update Knight.java
    Fixed moves

--- src/main/java/Group3/Chess/Knight.java
+++ src/main/java/Group3/Chess/Knight.java
@@ -14,7 +14,7 @@ public class Knight extends Piece {
 	public List<Square> getMoves() {
 		List<Square> squares = new ArrayList<Square>();		
 		//finds the possible squares up up right
-		if (getSquare().getY() > 1 && getSquare().getX() < 6) 
+		if (getSquare().getY() > 1 && getSquare().getX() < 7) 
 		{
 			Square square = getBoard().getSquare(getSquare().getX() + 1, getSquare().getY() - 2);
 			if (square.getPiece() == null) {
@@ -24,7 +24,7 @@ public class Knight extends Piece {
 			}
 		}
 		//finds the possible squares up right right
-		if (getSquare().getY() > 0 && getSquare().getX() < 5) 
+		if (getSquare().getY() > 0 && getSquare().getX() < 6) 
 		{
 			Square square = getBoard().getSquare(getSquare().getX() + 2, getSquare().getY() - 1);
 			if (square.getPiece() == null) {
@@ -85,7 +85,7 @@ public class Knight extends Piece {
 			}
 		}
 		//finds the possible squares down left left
-		if (getSquare().getY() < 6 && getSquare().getX() > 1)		
+		if (getSquare().getY() <= 6 && getSquare().getX() > 1)		
 		{
 			Square square = getBoard().getSquare(getSquare().getX() - 2, getSquare().getY() + 1);
 			if (square.getPiece() == null) {
